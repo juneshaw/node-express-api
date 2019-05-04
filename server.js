c485925261a0f4a2bd40d93ff6228572110ec377
June Shaw <june.shaw@me.com>
2019-05-04 14:52:40 -0600

    Books route without router

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/error', (req, res) => {
     res.send('Error from API!');
 });
 
+app.get('/books', (req, res) => {
+    res.send('Books');
+});
+
 // Listen for requests on environment port if specified, or 8080
 const PORT = process.env.PORT || 8080;
 
