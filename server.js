98546663acfe32d61efd2a683be64a9a9e81d63c
June Shaw <june.shaw@me.com>
2019-05-04 13:13:31 -0600

    Server, dotenv, git ignore, and package changes for basic app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+console.log('process env: ', process.env);
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+});
+
+// Listen for requests on environment port if specified, or 8080
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`I am listening on port ${PORT}`);
+})
