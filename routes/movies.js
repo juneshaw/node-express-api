b83f25132d939908dfa28bf5cbe8bc4d46c9db43
June Shaw <june.shaw@me.com>
2019-05-14 15:54:41 -0600

    Movie show with rating

--- routes/movies.js
+++ routes/movies.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const pageSize = 50;
 
-function connectDb() {
+function connectDb(databaseName) {
     return new Promise(function(resolve, reject) {
-        let db = new sqlite3.Database('./db/movies.db', sqlite3.OPEN_READWRITE, (err) => {
+        let db = new sqlite3.Database(databaseName, sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             reject(new Error('Failed database connect'));
             console.error(err.message);
@@ -21,15 +21,15 @@ router.get('/', function(req, res, next) {
     0;
   const pageLimit = req.query.page ? pageSize : -1;
   const queryString = `SELECT imdbId, title, genres, releaseDate, printf ('$%d', budget) AS budget FROM movies LIMIT ${pageLimit} OFFSET ${pageOffset}`
-  console.log('query string: ', queryString);
-  connectDb()
+  connectDb('./db/movies.db')
   .then(function (db) {
     db.all(queryString, function(err, movies) {
       if (err) {
         console.error(err.message);
+      } else {
+        // res.render({movies});
+        res.send({movies});
       }
-      // res.render({movies});
-      res.send({movies});
     });
   })
   .catch(function(err) {
@@ -38,21 +38,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-  const queryString = `SELECT imdbId, title, genres, releaseDate, printf ('$%d', budget) AS budget FROM movies WHERE movieId = ${req.params.id}`
-  console.log('query string show movie: ', queryString);
-  connectDb()
+  const queryString = `SELECT imdbId, title, genres, releaseDate, printf ('$%d', budget) AS budget FROM movies INNER JOIN ratings on ratings.movieId = movies.movieId WHERE movies.movieId = ${req.params.id}`
+  connectDb('./db/movies.db')
   .then(function (db) {
-    db.all(queryString, function(err, movies) {
-      if (err) {
-                console.error(err.message);
-              }
-            // res.render({movies});
-            res.send({movies});
-            });
-        })
-        .catch(function(err) {
-            console.error('Error db connect')
+    db.serialize(() => {
+      // Queries scheduled here will be serialized.
+      db.run(`ATTACH DATABASE "./db/ratings.db" AS "ratings"`)
+        .get(queryString, (err, movie) => {
+          if (err) {
+            console.error(err.message);
+          } else {
+            // res.render({movie});
+            res.send({movie});
+          }
         });
-    });
+    })
+  })
+  .catch(function(err) {
+    console.error('Error db connect', err);
+  });
+  // close the database connection
+  // db.close((err) => {
+  //   if (err) {
+  //     return console.error(err.message);
+  //   }
+  // });
+});
 
 module.exports = router;
