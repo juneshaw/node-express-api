143c5e25788adb77b32add220f79bd26be66d1be
June Shaw <june.shaw@me.com>
2019-05-14 07:09:29 -0600

    Movie show without rating

--- routes/movies.js
+++ routes/movies.js
@@ -21,30 +21,32 @@ router.get('/', function(req, res, next) {
     0;
   const pageLimit = req.query.page ? pageSize : -1;
   const queryString = `SELECT imdbId, title, genres, releaseDate, printf ('$%d', budget) AS budget FROM movies LIMIT ${pageLimit} OFFSET ${pageOffset}`
+  console.log('query string: ', queryString);
   connectDb()
-      .then(function (db) {
-          console.log('query string: ', queryString);
-          db.all(queryString, function(err, movies) {
-            if (err) {
-              console.error(err.message);
-            }
-          // res.render({movies});
-          res.send({movies});
-          });
-      })
-      .catch(function(err) {
-          console.error('Error db connect')
-      });
+  .then(function (db) {
+    db.all(queryString, function(err, movies) {
+      if (err) {
+        console.error(err.message);
+      }
+      // res.render({movies});
+      res.send({movies});
+    });
+  })
+  .catch(function(err) {
+    console.error('Error db connect')
   });
+});
 
-  router.get('/:id', function(req, res, next) {
-    connectDb()
-        .then(function (db) {
-            db.all("SELECT * FROM movies where id = " + req.params.id, function(err, movie) {
-              if (err) {
+router.get('/:id', function(req, res, next) {
+  const queryString = `SELECT imdbId, title, genres, releaseDate, printf ('$%d', budget) AS budget FROM movies WHERE movieId = ${req.params.id}`
+  console.log('query string show movie: ', queryString);
+  connectDb()
+  .then(function (db) {
+    db.all(queryString, function(err, movies) {
+      if (err) {
                 console.error(err.message);
               }
-            // res.render({movie});
+            // res.render({movies});
             res.send({movies});
             });
         })
